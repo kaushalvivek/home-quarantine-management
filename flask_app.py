7e44021add147a0fab7cc277deaaf5855294670f
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2020-07-31 16:22:48 +0530

    all fuctionality except for visualisations

--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, url_for, redirect, request, jsonify, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import random , string
 import pandas as pd
 import configparser
@@ -44,8 +44,14 @@ class OperatorLog(db.Model):
 
 class PatientLog(db.Model):
   tran_id = db.Column(db.String, primary_key=True)
+  date = db.Column(db.DateTime)
   pid = db.Column(db.String)
   symptoms = db.Column(db.String)
+
+class Symptoms(db.Model):
+  symptom_id = db.Column(db.String, primary_key=True)
+  name = db.Column(db.String)
+  date_created = db.Column(db.DateTime)
 
 # hourly automated in backend:
 # - fill current patients
@@ -57,12 +63,14 @@ def index():
   # if operator is logged in
   if "operator" in session:
     # get patients using op_id
+    symptoms = Symptoms.query.all()
     op_id = session['operator'].op_id
     patients_table = pd.read_sql("SELECT * from current_patients where op_id=="+op_id+";", db.session.bind)
     if patients_table.shape[0] == 0:
       return render_template('operator_login.html', wrong=False)
     to_contact_patients = []
     contacted_patients = []
+    unreachable_patients = []
     for i in range(0,patients_table.shape[0]):
       patient = {
         'pid':patients_table.iloc[i,0],
@@ -75,17 +83,13 @@ def index():
       }
       if patients_table.iloc[i,7] == "no":
         to_contact_patients.append(patient)
-      else:
+      elif patients_table.iloc[i,7] == "contacted":
         contacted_patients.append(patient)
+      else:
+        unreachable_patients.append(patient)
     to_contact = len(to_contact_patients)
-    contact_list = patients_table['contacted'].tolist()
-    contacted = 0
-    unreachable = 0
-    for i in contact_list:
-      if i == "contacted":
-        contacted+=1
-      elif i == "unreachable":
-        unreachable+=1
+    contacted = len(contacted_patients)
+    unreachable = len(unreachable_patients)
     
     return render_template('index.html',
     to_contact_patients=to_contact_patients,
@@ -93,19 +97,37 @@ def index():
      contacted=contacted,
      unreachable=unreachable,
      name=session['operator'].name,
-     contacted_patients = contacted_patients
+     unreachable_patients = unreachable_patients,
+     symptoms = symptoms
      )
   else:
     return render_template('operator_login.html', wrong=False)
 
-# mark patients as contacted/unreachable
+# mark patients as contacted
 @app.route('/contact')
 def contact():
   if "operator" in session:
-    status = request.args.get('status')
+    tran_id = random.randint(1000000000,9999999999)
+    pid = request.args.get('pid')
+    symptoms = request.args.get('symptoms')
+    print(symptoms)
+    created = datetime.now()
+    new_patient_log = PatientLog(tran_id=tran_id, pid=pid,date=created,symptoms=symptoms)
+    db.session.add(new_patient_log)
+    patient = CurrentPatients.query.filter_by(pid=pid).first()
+    patient.contacted = "contacted"
+    db.session.commit()
+    return redirect('/')
+  else:
+    return render_template('operator_login.html', wrong=False)
+
+# mark patients as unreachable
+@app.route('/unreachable')
+def unreachable():
+  if "operator" in session:
     pid = request.args.get('pid')
     patient = CurrentPatients.query.filter_by(pid=pid).first()
-    patient.contacted = status
+    patient.contacted = "unreachable"
     db.session.commit()
     return redirect('/')
   else:
@@ -150,8 +172,9 @@ def operator_verify():
 @app.route('/admin_page')
 def admin_page():
   operator = Operators.query.all()
+  symptoms = Symptoms.query.all()
   if session.get("signin_key") == 'sadewiapexfdiblrnnwuludogkqyuazp':
-    return render_template('admin_page.html', operator=operator)
+    return render_template('admin_page.html', operator=operator, symptoms=symptoms)
   else:
     return render_template('admin_login.html',wrong=False)
 
@@ -163,7 +186,8 @@ def create_operator():
   else:
     return render_template('admin_login.html',wrong=False)
 
-# page to delete new operators
+
+# route to delete operators
 @app.route('/delete_operator')
 def delete_operator():
   if session.get("signin_key") == 'sadewiapexfdiblrnnwuludogkqyuazp':
@@ -173,6 +197,18 @@ def delete_operator():
     return redirect('/admin_page')
   else:
     return render_template('admin_login.html',wrong=False)
+
+# route to delete symptoms
+@app.route('/delete_symptom')
+def delete_symptom():
+  if session.get("signin_key") == 'sadewiapexfdiblrnnwuludogkqyuazp':
+    symptom_id = request.args.get('symptom_id')
+    Symptoms.query.filter_by(symptom_id=symptom_id).delete()
+    db.session.commit()
+    return redirect('/admin_page')
+  else:
+    return render_template('admin_login.html',wrong=False)
+
 
 # commit operator details
 @app.route('/get_operator_data')
@@ -185,6 +221,20 @@ def get_operator_data():
     op_id = random.randint(100000,999999)
     new_op = Operators(name=name, email=email, password=password, date_created=date_created, op_id=op_id)
     db.session.add(new_op)
+    db.session.commit()
+    return redirect("/admin_page")
+  else:
+    return render_template('admin_login.html',wrong=False)
+
+# commit operator details
+@app.route('/get_symptom')
+def get_symptom():
+  if session.get("signin_key") == 'sadewiapexfdiblrnnwuludogkqyuazp':
+    name = request.args.get('name')
+    symptom_id = random.randint(100000,999999)
+    date_created = datetime.now()
+    new_symptom = Symptoms(name=name, date_created=date_created, symptom_id=symptom_id)
+    db.session.add(new_symptom)
     db.session.commit()
     return redirect("/admin_page")
   else:
